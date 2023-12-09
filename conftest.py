4594872384812c9b45f36f7c22bf6f0ef170abf9
serig85 <115493415+serig85@users.noreply.github.com>
2023-12-09 12:28:18 +0300

    4.1 step 6

---
+++ conftest.py
@@ -0,0 +1,15 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+@pytest.fixture(scope="function")
+def browser(request):
+    print("\nstart browser for test..")
+    browser = webdriver.Chrome()
+    yield browser
+    print("\nquit browser..")
+    browser.quit()
+
+def pytest_addoption(parser):
+    parser.addoption('--language', action='store', default='en',
+                     help="Choose language")
